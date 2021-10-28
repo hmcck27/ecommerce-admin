880ed3601b9b3318b56df98fdbe2ecad13ab977d
backend <backend@JK.local>
2021-10-28 15:33:23 +0900

    initial commit

---
+++ src/test/java/springecommerce/ecommerceadmin/EcommerceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package springecommerce.ecommerceadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
